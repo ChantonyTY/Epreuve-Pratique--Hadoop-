b45267fd8c4f81bc9d6d9dce1de89b3ceb4c7562
ChantonyTY <74354624+ChantonyTY@users.noreply.github.com>
2021-12-21 18:42:13 +0100

    Update hadoop-env.sh

--- hadoop-2.10.1/etc/hadoop/hadoop-env.sh
+++ hadoop-2.10.1/etc/hadoop/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk 
+export JAVA_HOME=/usr/local/jdk-1.8
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
